613d95000411160b5ca9734f385c76d74d337102
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-02-12 12:08:20 +0100

    react-bootstraptest for grids

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,94 +1,101 @@
-import { Container, Row, Col } from 'reactstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap.scss';
 
 const App = () => {
   return (
-    <Container>
+    <Container fluid>
       <Row>
-        <Col className="bg-light border">.col</Col>
-      </Row>
-      <Row>
-        <Col className="bg-light border">.col</Col>
-        <Col className="bg-light border">.col</Col>
-        <Col className="bg-light border">.col</Col>
-        <Col className="bg-light border">.col</Col>
-      </Row>
-      <Row>
-        <Col className="bg-light border" xs="3">
-          .col-3
-        </Col>
-        <Col className="bg-light border" xs="auto">
-          .col-auto - variable width content
-        </Col>
-        <Col className="bg-light border" xs="3">
-          .col-3
-        </Col>
-      </Row>
-      <Row>
-        <Col className="bg-light border" xs="6">
-          .col-6
-        </Col>
-        <Col className="bg-light border" xs="6">
-          .col-6
-        </Col>
-      </Row>
-      <Row>
-        <Col className="bg-light border" sm="4" xs="6">
-          .col-6 .col-sm-4
-        </Col>
-        <Col className="bg-light border" sm="4" xs="6">
-          .col-6 .col-sm-4
-        </Col>
-        <Col className="bg-light border" sm="4">
-          .col-sm-4
-        </Col>
-      </Row>
-      <Row>
-        <Col
-          className="bg-light border"
-          sm={{
-            offset: 1,
-            order: 2,
-            size: 6,
-          }}
-        >
-          .col-sm-6 .order-sm-2 .offset-sm-1
-        </Col>
-      </Row>
-      <Row>
-        <Col
-          className="bg-light border"
-          md={{
-            offset: 3,
-            size: 6,
-          }}
-          sm="12"
-        >
-          .col-sm-12 .col-md-6 .offset-md-3
-        </Col>
-      </Row>
-      <Row>
-        <Col
-          className="bg-light border"
-          sm={{
-            offset: 1,
-            size: 'auto',
-          }}
-        >
-          .col-sm-auto .offset-sm-1
-        </Col>
-        <Col
-          className="bg-light border"
-          sm={{
-            offset: 1,
-            size: 'auto',
-          }}
-        >
-          .col-sm-auto .offset-sm-1
-        </Col>
+        <Col>1 of 1</Col>
       </Row>
     </Container>
+    // <Container>
+    //   <Row>
+    //     <Col className="bg-light border">.col</Col>
+    //   </Row>
+    //   <Row>
+    //     <Col className="bg-light border">.col</Col>
+    //     <Col className="bg-light border">.col</Col>
+    //     <Col className="bg-light border">.col</Col>
+    //     <Col className="bg-light border">.col</Col>
+    //   </Row>
+    //   <Row>
+    //     <Col className="bg-light border" xs="3">
+    //       .col-3
+    //     </Col>
+    //     <Col className="bg-light border" xs="auto">
+    //       .col-auto - variable width content
+    //     </Col>
+    //     <Col className="bg-light border" xs="3">
+    //       .col-3
+    //     </Col>
+    //   </Row>
+    //   <Row>
+    //     <Col className="bg-light border" xs="6">
+    //       .col-6
+    //     </Col>
+    //     <Col className="bg-light border" xs="6">
+    //       .col-6
+    //     </Col>
+    //   </Row>
+    //   <Row>
+    //     <Col className="bg-light border" sm="4" xs="6">
+    //       .col-6 .col-sm-4
+    //     </Col>
+    //     <Col className="bg-light border" sm="4" xs="6">
+    //       .col-6 .col-sm-4
+    //     </Col>
+    //     <Col className="bg-light border" sm="4">
+    //       .col-sm-4
+    //     </Col>
+    //   </Row>
+    //   <Row>
+    //     <Col
+    //       className="bg-light border"
+    //       sm={{
+    //         offset: 1,
+    //         order: 2,
+    //         size: 6,
+    //       }}
+    //     >
+    //       .col-sm-6 .order-sm-2 .offset-sm-1
+    //     </Col>
+    //   </Row>
+    //   <Row>
+    //     <Col
+    //       className="bg-light border"
+    //       md={{
+    //         offset: 3,
+    //         size: 6,
+    //       }}
+    //       sm="12"
+    //     >
+    //       .col-sm-12 .col-md-6 .offset-md-3
+    //     </Col>
+    //   </Row>
+    //   <Row>
+    //     <Col
+    //       className="bg-light border"
+    //       sm={{
+    //         offset: 1,
+    //         size: 'auto',
+    //       }}
+    //     >
+    //       .col-sm-auto .offset-sm-1
+    //     </Col>
+    //     <Col
+    //       className="bg-light border"
+    //       sm={{
+    //         offset: 1,
+    //         size: 'auto',
+    //       }}
+    //     >
+    //       .col-sm-auto .offset-sm-1
+    //     </Col>
+    //   </Row>
+    // </Container>
   );
 };
 
