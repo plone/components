7257875a5e68d73947817a7baee77c418bbf4036
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-10-27 18:23:28 +0200

    Add styling for breadcrumbs, improve component.

--- src/components/Breadcrumbs/Breadcrumb.stories.tsx
+++ src/components/Breadcrumbs/Breadcrumb.stories.tsx
@@ -22,8 +22,20 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
 export const Default: Story = {
   args: {
+    root: '/',
+    includeRoot: true,
     items: [
-      { '@id': '/', title: 'Home' },
+      { '@id': '/folder', title: 'Folder' },
+      { '@id': '/folder/page', title: 'Page' },
+    ],
+  },
+};
+
+export const NoRoot: Story = {
+  args: {
+    root: '/',
+    includeRoot: false,
+    items: [
       { '@id': '/folder', title: 'Folder' },
       { '@id': '/folder/page', title: 'Page' },
     ],
--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -3,20 +3,50 @@ import {
   Breadcrumb,
   Link,
 } from 'react-aria-components';
-import type { BreadcrumbsProps } from 'react-aria-components';
+import HomeIcon from './HomeIcon';
+import type { BreadcrumbsProps as RACBreadcrumbsProps } from 'react-aria-components';
 
 type Breadcrumb = {
   '@id': string;
   title: string;
 };
 
-export default function Breadcrumbs({ items }: BreadcrumbsProps<Breadcrumb>) {
+interface BreadcrumbsProps<T> extends RACBreadcrumbsProps<T> {
+  /**
+   * Current navigation root URL (flattened)
+   */
+  root?: string;
+  /**
+   * Whether include the root item in the breadcrubs (based on the root prop)
+   */
+  includeRoot?: boolean;
+}
+
+/**
+ * Breadcrumbs display a hierarchy of links to the current page or resource in an application.
+ */
+export default function Breadcrumbs({
+  items,
+  root,
+  includeRoot,
+}: BreadcrumbsProps<Breadcrumb>) {
+  let itemsWithRoot: typeof items;
+  if (includeRoot) {
+    const rootItem: Breadcrumb = {
+      '@id': root || '/',
+      title: 'Home',
+    };
+    itemsWithRoot = [rootItem, ...(items as Breadcrumb[])];
+  }
   return (
     <nav aria-label="breadcrumbs" role="navigation">
-      <RACBreadcrumbs className="q breadcrumbs" items={items}>
+      <RACBreadcrumbs className="q breadcrumbs" items={itemsWithRoot || items}>
         {(item) => (
           <Breadcrumb key={item['@id']} className="q breadcrumb">
-            <Link href={item['@id']}>{item.title}</Link>
+            <Link href={item['@id']}>
+              {item['@id'] === (root || '/') && <HomeIcon size="S" />}
+              {item.title}
+            </Link>
           </Breadcrumb>
         )}
       </RACBreadcrumbs>
