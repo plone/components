a21b65f38d799b04752fbc5ad206c18adcfded73
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-10-31 20:45:31 +0100

    Add storybook for link and a11 test

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -1,19 +1,15 @@
-import { render } from '@testing-library/react';
+import { expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import Link from './Link';
 
 expect.extend(toHaveNoViolations);
 
-it('Breadcrumbs basic a11y test', async () => {
-  const { container } = render(
-    <Breadcrumbs
-      items={[
-        { '@id': '/', title: 'Home' },
-        { '@id': '/folder', title: 'Folder' },
-        { '@id': '/folder/page', title: 'Page' },
-      ]}
-    />,
-  );
+it('Link basic a11y test', async () => {
+  const { container } = render(<Link href="/">The link</Link>);
+
+  const asd = screen.getByText('The link');
+  expect(asd).toHaveAttribute('href', '/');
 
   const results = await axe(container);
 
