0c380a53c9b7e9913b8b201577825860a53d8a19
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-11-01 18:43:55 +0100

    Fix storybook

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true }), react()],
+  plugins: [dts(), react()],
   build: {
     lib: {
       entry: [path.resolve(__dirname, 'src/index.ts')],
